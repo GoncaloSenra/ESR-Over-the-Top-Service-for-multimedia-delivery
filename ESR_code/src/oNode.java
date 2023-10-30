a5049d5f0f4fc60323513dd78132f8788fb3bcbf
GoncaloSenra <goncalosenra@gmail.com>
2023-10-30 20:25:08 +0000

    Threads

--- ESR_code/src/oNode.java
+++ ESR_code/src/oNode.java
@@ -8,13 +8,21 @@ public class oNode {
 
     public static String name;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         name = args[0];
         oNode o = new oNode();
 
+        Thread thread1 = new Thread(() -> {
+            o.hello();
+        });
+
+
+
+        // Inicia as threads
+        thread1.start();
     }
 
-    public void parseConfigFile(String name) {
+    private void parseConfigFile(String name) {
 
         try {
 
@@ -41,78 +49,90 @@ public class oNode {
 
     }
 
-    public oNode() throws Exception {
+    public oNode() {
+        this.parseConfigFile(name);
+    }
 
-        parseConfigFile(name);
 
+    private void hello() {
 
-        DatagramSocket serverSocket = new DatagramSocket(9876);
-        while (true) {
-            byte[] receiveData = new byte[1024];
-            byte[] sendData = new byte[1024];
+        try {
 
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            serverSocket.receive(receivePacket);
-            
-            byte[] data = receivePacket.getData();
-            
+            DatagramSocket serverSocket = new DatagramSocket(9876);
+            while (true) {
+                byte[] receiveData = new byte[1024];
+                byte[] sendData = new byte[1024];
+    
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                
+                byte[] data = receivePacket.getData();
+                
+    
+                ByteArrayInputStream bais = new ByteArrayInputStream(data);
+                ObjectInputStream ois = new ObjectInputStream(bais);
 
-            ByteArrayInputStream bais = new ByteArrayInputStream(data);
-            ObjectInputStream ois = new ObjectInputStream(bais);
-            try {
-                Object readObject = ois.readObject();
-                if (readObject instanceof Packet) {
-                    Packet p = (Packet) readObject;
-                    String str = p.getData();
-                    InetAddress IPAddress = InetAddress.getByName(receivePacket.getAddress().toString().replace("/", ""));
-                    p.setPath(IPAddress);
-
-                    for (InetAddress address : p.getPrevNetworks()) {
-                        if (!p.getNetworks().contains(address)) {
-                            p.getNetworks().add(address);
-                        }
-                    }
-                    p.setPrevNetworksZero();
-                    
-                    for (IpWithMask ip : ipVizinhos) {
-                            p.setPrevNetworks(ip.getNetwork());
-                    }
-
-                    System.out.println("RECEIVED: " + str + " from " + receivePacket.getAddress() + ":" + 9876);
-                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                    ObjectOutputStream oos = new ObjectOutputStream(baos);
-                    oos.writeObject(p);
-                    oos.close();
-                    byte[] datak = baos.toByteArray();
-
-                    System.out.println("====================================");
-                    for (InetAddress ip : p.getNetworks()) {
-                        System.out.println("->" + ip.getHostAddress());
-                    }
-                    System.out.println("====================================");
-
-                    for (IpWithMask ip : ipVizinhos) {
-                        InetAddress packetNetwork = ip.getNetwork();
-                        boolean sent = false; // Variável para verificar se o pacote foi enviado
-                        //System.out.print("IP: " + packetNetwork.getHostAddress() + " -> " );
-                        for (InetAddress ip_network : p.getNetworks()) {
-                            //System.out.println("IP_NETWORK: " + ip_network);
-                            if (ip_network.getHostAddress().equals(packetNetwork.getHostAddress())){
-                                sent = true;
-                                break;
+                try {
+                    Object readObject = ois.readObject();
+                    if (readObject instanceof Packet) {
+                        Packet p = (Packet) readObject;
+                        String str = p.getData();
+                        InetAddress IPAddress = InetAddress.getByName(receivePacket.getAddress().toString().replace("/", ""));
+                        p.setPath(IPAddress);
+    
+                        for (InetAddress address : p.getPrevNetworks()) {     
+                            if (!p.getNetworks().contains(address)) {
+                                p.getNetworks().add(address);
                             }
                         }
-                    
-                        if (!sent) {
-                            DatagramPacket sendPacket = new DatagramPacket(datak, datak.length, ip.getAddress(), 9876);
-                            serverSocket.send(sendPacket);
-                            System.out.println("SENT: " + str + " to " + ip.getAddress() + ":" + 9876);
+                        p.setPrevNetworksZero();
+                        
+                        for (IpWithMask ip : ipVizinhos) {
+                                p.setPrevNetworks(ip.getNetwork());
                         }
+    
+                        System.out.println("RECEIVED: " + str + " from " + receivePacket.getAddress() + ":" + 9876);
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        ObjectOutputStream oos = new ObjectOutputStream(baos);
+                        oos.writeObject(p);
+                        oos.close();
+                        byte[] datak = baos.toByteArray();
+    
+                        System.out.println("====================================");
+                        for (InetAddress ip : p.getNetworks()) {
+                            System.out.println("->" + ip.getHostAddress());
+                        }
+                        System.out.println("====================================");
+    
+                        for (IpWithMask ip : ipVizinhos) {
+                            InetAddress packetNetwork = ip.getNetwork();
+                            boolean sent = false; // Variável para verificar se o pacote foi enviado
+                            //System.out.print("IP: " + packetNetwork.getHostAddress() + " -> " );
+                            for (InetAddress ip_network : p.getNetworks()) {
+                                //System.out.println("IP_NETWORK: " + ip_network);
+                                if (ip_network.getHostAddress().equals(packetNetwork.getHostAddress())){
+                                    sent = true;
+                                    break;
+                                }
+                            }
+                        
+                            if (!sent) {
+                                DatagramPacket sendPacket = new DatagramPacket(datak, datak.length, ip.getAddress(), 9876);
+                                serverSocket.send(sendPacket);
+                                System.out.println("SENT: " + str + " to " + ip.getAddress() + ":" + 9876);
+                            }
+                        }
+
                     }
+                } catch (ClassNotFoundException e3) {
+                    e3.printStackTrace();
                 }
-            }catch(Exception e){
-                e.printStackTrace();
+
             }
+        } catch (SocketException e1) {
+            e1.printStackTrace();
+        } catch (IOException e2) {
+            e2.printStackTrace();
         }
     }
 }
