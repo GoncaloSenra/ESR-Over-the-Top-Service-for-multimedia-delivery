23fb851a91f9a3baf16da98cfb72afabfa89cf7c
Rikes8 <henrique19costa@gmail.com>
2023-10-31 17:57:48 +0000

    pings a arder

--- ESR_code/src/oNode.java
+++ ESR_code/src/oNode.java
@@ -1,10 +1,10 @@
 import java.io.*;
 import java.net.*;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class oNode {
 
-    public ConcurrentLinkedQueue<IpWithMask> ipVizinhos;
+    public ConcurrentHashMap<IpWithMask, Boolean> activeRouters;
 
     public static String name;
 
@@ -16,18 +16,27 @@ public class oNode {
             o.hello();
         });
 
+        Thread thread2 = new Thread(() -> {
+            o.ping();
+        });
 
+        Thread thread3 = new Thread(() -> {
+            o.pong();
+        });
 
         // Inicia as threads
         thread1.start();
-    }
+        thread2.start();
+        thread3.start();
+    }   
 
     private void parseConfigFile(String name) {
 
         try {
 
-            ipVizinhos = new ConcurrentLinkedQueue<IpWithMask>();
-
+            //ipVizinhos = new ConcurrentLinkedQueue<IpWithMask>();
+            activeRouters = new ConcurrentHashMap<IpWithMask, Boolean>();
+            
             // System.out.println(System.getProperty("user.dir"));
             // System.out.println("p"+name +".txt");
             FileReader f = new FileReader(name + ".txt");
@@ -35,13 +44,13 @@ public class oNode {
 
             String ip;
             while ((ip = buffer.readLine()) != null) {
-                ipVizinhos.offer(new IpWithMask(ip));
+                activeRouters.put(new IpWithMask(ip), false);
             }
 
             f.close();
 
             // debug
-            System.out.println("Ip's na lista: " + ipVizinhos.toString());
+            System.out.println("Ip's na lista: " + activeRouters.toString());
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Erro ao ler arquivo de configuracao");
@@ -58,7 +67,7 @@ public class oNode {
 
         try {
 
-            DatagramSocket serverSocket = new DatagramSocket(9876);
+            DatagramSocket serverSocket = new DatagramSocket(9000);
             while (true) {
                 byte[] receiveData = new byte[1024];
                 byte[] sendData = new byte[1024];
@@ -87,8 +96,10 @@ public class oNode {
                         }
                         p.setPrevNetworksZero();
                         
-                        for (IpWithMask ip : ipVizinhos) {
-                                p.setPrevNetworks(ip.getNetwork());
+                        for (ConcurrentHashMap.Entry<IpWithMask, Boolean> entry : activeRouters.entrySet()) {
+                            IpWithMask key = entry.getKey();
+                            Boolean value = entry.getValue();
+                            System.out.println("Clave: " + key + ", Valor: " + value);
                         }
     
                         System.out.println("RECEIVED: " + str + " from " + receivePacket.getAddress() + ":" + 9876);
@@ -104,22 +115,27 @@ public class oNode {
                         }
                         System.out.println("====================================");
     
-                        for (IpWithMask ip : ipVizinhos) {
-                            InetAddress packetNetwork = ip.getNetwork();
-                            boolean sent = false; // Variável para verificar se o pacote foi enviado
-                            //System.out.print("IP: " + packetNetwork.getHostAddress() + " -> " );
-                            for (InetAddress ip_network : p.getNetworks()) {
-                                //System.out.println("IP_NETWORK: " + ip_network);
-                                if (ip_network.getHostAddress().equals(packetNetwork.getHostAddress())){
-                                    sent = true;
-                                    break;
+                        //for (IpWithMask ip : activeRouters.) {
+                        for (ConcurrentHashMap.Entry<IpWithMask, Boolean> entry : activeRouters.entrySet()) {
+                            //IpWithMask key = entry.getKey();
+                            //Boolean value = entry.getValue();
+                            if(entry.getValue()){
+                                InetAddress packetNetwork = entry.getKey().getNetwork();
+                                boolean sent = false; // Variável para verificar se o pacote foi enviado
+                                //System.out.print("IP: " + packetNetwork.getHostAddress() + " -> " );
+                                for (InetAddress ip_network : p.getNetworks()) {
+                                    //System.out.println("IP_NETWORK: " + ip_network);
+                                    if (ip_network.getHostAddress().equals(packetNetwork.getHostAddress())){
+                                        sent = true;
+                                        break;
+                                    }
                                 }
-                            }
-                        
-                            if (!sent) {
-                                DatagramPacket sendPacket = new DatagramPacket(datak, datak.length, ip.getAddress(), 9876);
-                                serverSocket.send(sendPacket);
-                                System.out.println("SENT: " + str + " to " + ip.getAddress() + ":" + 9876);
+                            
+                                if (!sent) {
+                                    DatagramPacket sendPacket = new DatagramPacket(datak, datak.length, entry.getKey().getAddress(), 9876);
+                                    serverSocket.send(sendPacket);
+                                    System.out.println("SENT: " + str + " to " + entry.getKey().getAddress() + ":" + 9876);
+                                }
                             }
                         }
 
@@ -134,5 +150,88 @@ public class oNode {
         } catch (IOException e2) {
             e2.printStackTrace();
         }
+    }
+
+    private void ping(){
+        
+        try {
+            DatagramSocket pingSocket = new DatagramSocket(8000);
+            pingSocket.setSoTimeout(1000);
+            while (true) {
+                byte[] receiveData = new byte[1024];
+                byte[] sendData = new byte[1024];
+                byte[] data = new byte[1024];
+
+                data = "PING".getBytes();
+
+                for (ConcurrentHashMap.Entry<IpWithMask, Boolean> entry : activeRouters.entrySet()) {
+                    
+                    DatagramPacket sendPacket = new DatagramPacket(data, data.length, entry.getKey().getAddress(), 8500);
+                    pingSocket.send(sendPacket);
+                    System.out.println("PING enviado para: " + entry.getKey().getAddress().getHostName());
+                    
+                    
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    pingSocket.receive(receivePacket);
+                    
+                    
+                    // Caiu no timeout
+                    if (receivePacket.getData() == null) {
+                        System.out.println("------TIMEOUT : " + entry.getKey().getAddress().getHostName() + " -----");
+                        entry.setValue(false);
+                        System.out.println("TAbela ardes -> " + activeRouters.toString());
+                        continue;
+                    }
+            
+                    InetAddress addr = receivePacket.getAddress();                   
+
+                    if (addr.getHostName().equals(entry.getKey().getAddress().getHostName())) {
+                        System.out.println("PONG recebido de: " + entry.getKey().getAddress().getHostName());
+                        entry.setValue(true);
+                        System.out.println("Tabela recebeu PONG:" + activeRouters.toString());
+                    }
+                }
+                
+            }
+
+        } catch (SocketException e1) {
+            e1.printStackTrace();
+        } catch (IOException e2) {
+            e2.printStackTrace();
+        }
+        
+    }
+
+    private void pong(){
+        
+        try {
+            DatagramSocket pongSocket = new DatagramSocket(8500);
+
+            while (true) {
+                byte[] receiveData = new byte[1024];
+                byte[] sendData = new byte[1024];
+                
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                pongSocket.receive(receivePacket);
+                System.out.println("PING recebido! ->" + receivePacket.getAddress().getHostName());
+            
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ObjectOutputStream oos = new ObjectOutputStream(baos);
+                oos.writeObject("pong");
+                oos.close();
+                sendData = baos.toByteArray();
+
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), 8000);
+                pongSocket.send(sendPacket);
+                System.out.println("PONG enviado! ->" + receivePacket.getAddress().getHostName());
+            }
+            
+
+        } catch (SocketException e1) {
+            e1.printStackTrace();
+        } catch (IOException e2) {
+            e2.printStackTrace();
+        }
+        
     }
 }
